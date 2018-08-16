1a83d1e2f0f702736c2039e74d443cb5711cd029
Marta Sobstyl <marta.sobstyl@gmail.com>
2018-08-16 16:06:29 +0200

    commit

--- client/src/Login.js
+++ client/src/Login.js
@@ -14,13 +14,14 @@ function onC() {
 
 const Login = () => {
     return (
-    /*<GitHubLogin clientId="c5360afa40e2869bcb91" //my ClientID from GitHub
-                 redirectUri="http://localhost:8080/auth/github/callback"
+    <GitHubLogin clientId="c5360afa40e2869bcb91" //my ClientID from GitHub
+                 //redirectUri="http://localhost:8080/auth/github/callback"
+                 redirectUri="https://login-git-app.herokuapp.com"
                   //user will be redirected here e.g. http://localhost:8080/users/
                  onSuccess={onSuccess}
                  onFailure={onFailure}
-    />*/
-    <a href="https://github.com/login/oauth/authorize?client_id=c5360afa40e2869bcb91&redirect_uri=https://login-git-app.herokuapp.com/auth/github/callback">Login with github</a>
+    />
+    /*<a href="https://github.com/login/oauth/authorize?client_id=c5360afa40e2869bcb91&redirect_uri=https://login-git-app.herokuapp.com/auth/github/callback">Login with github</a>*/
     );
 };
 
